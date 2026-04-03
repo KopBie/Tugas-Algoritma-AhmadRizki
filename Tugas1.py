9718dda454f0285d0babd218c05a6c848e7f375e
KopBie <ahmadrizki0897@gmail.com>
2026-04-03 21:33:58 +0700

    belajar

---
+++ Tugas1.py
@@ -0,0 +1,8 @@
+namasales = str(input("Nama Sales: "))
+nilaipenjualan = float(input("Nilai Panjualan: "))
+
+komisi = 0.05*nilaipenjualan
+
+print ("Nama Sales: ", namasales)
+print ("Nilai Penjualan: ", nilaipenjualan)
+print ("Komisi: ", komisi)
