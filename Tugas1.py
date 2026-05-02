aee08c8811163c95e3f3367bca91df354e61f052
Ahmad Rizki <ahmadrizki0897@gmail.com>
2026-05-02 18:13:04 +0700

    Delete Tugas1.py

--- Tugas1.py
+++
@@ -1,8 +0,0 @@
-namasales = str(input("Nama Sales: "))
-nilaipenjualan = float(input("Nilai Panjualan: "))
-
-komisi = 0.05*nilaipenjualan
-
-print ("Nama Sales: ", namasales)
-print ("Nilai Penjualan: ", nilaipenjualan)
-print ("Komisi: ", komisi)
